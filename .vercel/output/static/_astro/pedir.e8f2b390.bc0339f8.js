7b625d83a48c653056cd41ff36d39b9dbbd6666b
Franco Itria <francoitria01@gmail.com>
2023-10-25 16:49:56 -0300

    npm run build

---
+++ .vercel/output/static/_astro/pedir.e8f2b390.bc0339f8.js
@@ -0,0 +1,1 @@
+const t="_card_5b93p_1",_="_title_5b93p_27",o="_price_5b93p_37",c="_buttons_5b93p_47",n="_total_5b93p_67",s="_button_5b93p_47",b={card:t,title:_,price:o,buttons:c,total:n,button:s};export{b as s};
