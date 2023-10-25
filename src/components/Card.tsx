9a11edd65a809777423dbce921cfa3502d9a1a71
Franco Itria <francoitria01@gmail.com>
2023-10-25 16:11:31 -0300

    card react component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,23 +1,33 @@
 import { add, priceDoc, priceMed, substract } from "../conts";
 import { useState } from "react";
 import style from "./Card.module.css"
-import React from '@astrojs/react'
 
 type props = {
   title: string;
   stock: boolean;
   option?: string;
+  setOrder: React.Dispatch<React.SetStateAction<{
+    'Carne salada': number;
+    'Carne dulce': number;
+    Pollo: number;
+    'Jamon y queso': number;
+    'Cebolla y queso': number;
+    Verdura: number;
+  }>>
+  setPriceTotal: React.Dispatch<React.SetStateAction<number>>
 };
 
-export function Card({ title, stock, option }: props) {
+export function Card({ title, stock, option, setOrder, setPriceTotal }: props) {
   const [total, setTotal] = useState<number>(0);
   const addTotal = () => {
-    console.log(add)
     setTotal((total: number) => total + 1);
+    setOrder(order => { return { ...order, [title]: total + 1 } })
+    setPriceTotal(price => price += priceDoc)
   };
   const substractTotal = () => {
-    console.log(substract)
     setTotal((total: number) => total - 1);
+    setOrder(order => { return { ...order, [title]: total - 1 } })
+    setPriceTotal(price => price -= priceDoc)
   };
   return (
     <div className={style.card}>
@@ -27,9 +37,9 @@ export function Card({ title, stock, option }: props) {
         <p className={style.price}>1/2: <b>${priceMed}</b></p>
       </div>
       <div className={style.buttons}>
-        <button onClick={addTotal} className={style.button}> + </button>
-        <p className={style.total}>{total}</p>
         <button onClick={substractTotal} className={style.button}> - </button>
+        <p className={style.total}>{total}</p>
+        <button onClick={addTotal} className={style.button}> + </button>
       </div>
     </div>
   )
