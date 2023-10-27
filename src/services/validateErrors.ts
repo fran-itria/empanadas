332ba98d6e2f63d36e5773e24de60cff56f11d0a
Franco Itria <francoitria01@gmail.com>
2023-10-27 04:58:51 -0300

    Complete logic form

--- src/services/validateErrors.ts
+++ src/services/validateErrors.ts
@@ -1,10 +1,8 @@
+import { inputDelyveryValues, inputsPaymentValues } from "../conts";
 import type { errorsForm } from "../types";
 import type { validateErrorsProps } from "./typesServices";
 
 export const validateErrors = ({ input, setErrors, price }: validateErrorsProps) => {
-    console.log(price)
-    console.log(input.cash)
-    
     let error: errorsForm = {}
 
     if (input.name.length == 0) {
@@ -19,7 +17,7 @@ export const validateErrors = ({ input, setErrors, price }: validateErrorsProps)
         error.delivery = 'Selecciona una forma de entrega'
     } else delete error.delivery
 
-    if (input.direction?.length == 0) {
+    if (input.delivery == inputDelyveryValues.home && input.direction?.length == 0) {
         error.direction = 'Complete su direccion'
     } else delete error.direction
 
@@ -27,7 +25,7 @@ export const validateErrors = ({ input, setErrors, price }: validateErrorsProps)
         error.payment = 'Seleccione una forma de pago'
     } else delete error.payment
 
-    if (Number(input.cash) < Number(price)) {
+    if (input.payment == inputsPaymentValues.efective && Number(input.cash) < Number(price)) {
         error.cash = 'Monto insuficiente'
     } else delete error.cash
 
