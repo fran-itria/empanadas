f6ef2afc0db857915f4e106d686bb59843ae23a6
Franco Itria <francoitria01@gmail.com>
2023-10-27 05:10:53 -0300

    clear address input and cash input when not selected

--- src/services/changeInputs.ts
+++ src/services/changeInputs.ts
@@ -1,9 +1,14 @@
+import { inputDelyveryValues, inputsNames, inputsPaymentValues } from "../conts"
 import type { ChangeInputs } from "./typesServices"
 import { validateErrors } from "./validateErrors"
 
 export const changeInputs = ({ inputs, event, setInputs, price, setErrors }: ChangeInputs) => {
     const name = event.currentTarget.name
     const value = event.currentTarget.value
-    setInputs(inputs => { return { ...inputs, [name]: value } })
-    validateErrors({input: {...inputs, [name]: value }, setErrors, price})
-}
+    if (name == inputsNames.delivery && value == inputDelyveryValues.search) {
+        setInputs(inputs => { return { ...inputs, [name]: value, [inputsNames.direction]: '' } })
+    } else if (name == inputsNames.payment && value == inputsPaymentValues.transfer) {
+        setInputs(inputs => { return { ...inputs, [name]: value, [inputsNames.cash]: '' } })
+    } else setInputs(inputs => { return { ...inputs, [name]: value } })
+    validateErrors({ input: { ...inputs, [name]: value }, setErrors, price })
+}   
