8d588930b70d1ae05524ca1a0eff5a407d6b18eb
Franco Itria <francoitria01@gmail.com>
2023-10-27 20:37:43 -0300

    added functions

--- src/services/addSubstract.ts
+++ src/services/addSubstract.ts
@@ -1,14 +1,31 @@
-import { priceDoc } from "../conts";
-import type { porpsAddSubstract } from "./typesServices";
+import { priceDoc, priceMed } from "../conts";
+import type { propsAddSubstractHalf, propsAddSubstractTotal } from "./typesServices";
 
-export const addTotal = ({ setOrder, setPriceTotal, setTotal, title, total}: porpsAddSubstract) => {
+
+export const addTotal = ({ basic, total, setTotal }: propsAddSubstractTotal) => {
+    const { setOrder, setPriceTotal, title } = basic
     setTotal((total: number) => total + 1);
-    setOrder(order => { return { ...order, [title]: total + 1 } })
+    setOrder((order) => { return { ...order, [title]: { ...order[title], doc: total + 1 } } })
     setPriceTotal(price => price += priceDoc)
 };
 
-export const substractTotal = ({ setOrder, setPriceTotal, setTotal, title, total}: porpsAddSubstract) => {
+export const substractTotal = ({ basic, total, setTotal }: propsAddSubstractTotal) => {
+    const { setOrder, setPriceTotal, title } = basic
     setTotal((total: number) => total - 1);
-    setOrder(order => { return { ...order, [title]: total - 1 } })
+    setOrder(order => { return { ...order, [title]: { ...order[title], doc: total - 1 } } })
     setPriceTotal(price => price -= priceDoc)
-  };
+};
+
+export const addHalf = ({ basic, half, setHalf }: propsAddSubstractHalf) => {
+    const { setOrder, setPriceTotal, title } = basic
+    setHalf((total: number) => total + 1);
+    setOrder(order => { return { ...order, [title]: { ...order[title], half: half + 1 } } })
+    setPriceTotal(price => price += priceMed)
+}
+
+export const substractHalf = ({ basic, half, setHalf }: propsAddSubstractHalf) => {
+    const { setOrder, setPriceTotal, title } = basic
+    setHalf((total: number) => total - 1);
+    setOrder(order => { return { ...order, [title]: { ...order[title], half: half - 1 } } })
+    setPriceTotal(price => price += priceMed)
+}
