01f6d43fb44ff4150d10d9be8d511d43ff82a078
Franco Itria <francoitria01@gmail.com>
2023-10-27 20:39:02 -0300

    styles card when out of stock

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,23 +1,54 @@
-import { priceDoc, priceMed } from "../conts";
+// import { priceDoc, priceMed } from "../conts";
 import { useState } from "react";
 import style from "./Card.module.css"
 import type { propsCard } from "../types";
-import { addTotal, substractTotal } from "../services/addSubstract";
+import { addHalf, addTotal, substractHalf, substractTotal } from "../services/addSubstract";
 
-export function Card({ title, stock, option, setOrder, setPriceTotal }: propsCard) {
+export function Card({ title, stock, setOrder, setPriceTotal }: propsCard) {
   const [total, setTotal] = useState<number>(0);
+  const [half, setHalf] = useState<number>(0)
   return (
-    <div className={style.card}>
+    <div className={stock ? style.card : style.notStock}>
       <h2 className={style.title}>{title}</h2>
-      <div className={style.prices}>
+      {/* <div className={style.prices}>
         <p className={style.price}>Docena: <b>${priceDoc}</b></p>
         <p className={style.price}>1/2: <b>${priceMed}</b></p>
-      </div>
-      <div className={style.buttons}>
-        <button onClick={() => substractTotal({ setOrder, setPriceTotal, setTotal, title, total })} className={style.button}> - </button>
+      </div> */}
+      <div className={style.doc}>
+        <p className={style.text}>Docenas:</p>
+        <button
+          onClick={() => substractTotal({ basic: { setOrder, setPriceTotal, title }, total, setTotal })}
+          className={total > 0 ? style.button : style.disbled}>
+          -
+        </button>
         <p className={style.total}>{total}</p>
-        <button onClick={() => addTotal({ setOrder, setPriceTotal, setTotal, title, total })} className={style.button}> + </button>
+        <button
+          onClick={() => addTotal({ basic: { setOrder, setPriceTotal, title }, total, setTotal })}
+          className={style.button}>
+          +
+        </button>
       </div>
+      <div className={style.midle}>
+        <p className={style.text}>1/2:</p>
+        <button
+          onClick={() => substractHalf({ basic: { setOrder, setPriceTotal, title }, half, setHalf })}
+          className={half > 0 ? style.button : style.disbled}>
+          -
+        </button>
+        <p className={style.total}>{half}</p>
+        <button
+          onClick={() => addHalf({ basic: { setOrder, setPriceTotal, title }, half, setHalf })}
+          className={style.button}>
+          +
+        </button>
+      </div>
+      {!stock ?
+        <>
+          <p className={style.notStockText}>Sin stock</p>
+          <div className={style.box}></div>
+          <div className={style.box2}></div>
+        </>
+        : <></>}
     </div>
   )
 } 
