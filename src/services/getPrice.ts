d317e460e30d60893539434d6a49be0d66202967
Franco Itria <francoitria01@gmail.com>
2023-10-27 04:57:01 -0300

    Modified function to get the total price value

--- src/services/getPrice.ts
+++ src/services/getPrice.ts
@@ -1,15 +1,14 @@
-import type { Price } from "../types";
-
-export const getPrice = (orderArray: string[] | undefined): Price => {
-    return orderArray?.filter((order) => {
+export const getPrice = (orderArray: string[] | undefined): string | void => {
+    let price = ''
+    const amount: string[] | undefined = orderArray?.filter((order) => {
         if (order.length > 0) {
             const text = order.split("=");
             const title = text[0];
             const amount: number = Number(text[1]);
-            if (amount > 0 && title == 'price')
-                return (
-                    amount
-                );
+            if (amount > 0 && title == 'price') return order
         }
     })
+    if (amount) {
+        return price = amount[0].split('=')[1]
+    }
 }
