9f31d2f5518752d126dc31e051d0ec6ad40c6ef2
Franco Itria <francoitria01@gmail.com>
2023-11-02 15:21:33 -0300

    Change function name

--- src/services/addSubstract.ts
+++ src/services/addSubstract.ts
@@ -2,14 +2,14 @@ import { priceDoc, priceMed } from "../conts";
 import type { propsAddSubstractHalf, propsAddSubstractTotal } from "./typesServices";
 
 
-export const addTotal = ({ basic, total, setTotal }: propsAddSubstractTotal) => {
+export const addDoc = ({ basic, total, setTotal }: propsAddSubstractTotal) => {
     const { setOrder, setPriceTotal, title } = basic
     setTotal((total: number) => total + 1);
     setOrder((order) => { return { ...order, [title]: { ...order[title], doc: total + 1 } } })
     setPriceTotal(price => price += priceDoc)
 };
 
-export const substractTotal = ({ basic, total, setTotal }: propsAddSubstractTotal) => {
+export const substractDoc = ({ basic, total, setTotal }: propsAddSubstractTotal) => {
     const { setOrder, setPriceTotal, title } = basic
     setTotal((total: number) => total - 1);
     setOrder(order => { return { ...order, [title]: { ...order[title], doc: total - 1 } } })
@@ -18,11 +18,9 @@ export const substractTotal = ({ basic, total, setTotal }: propsAddSubstractTota
 
 export const addHalf = ({ basic, half, setHalf }: propsAddSubstractHalf) => {
     const { setOrder, setPriceTotal, title } = basic
-    if (half < 1) {
         setHalf((total: number) => total + 1);
         setOrder(order => { return { ...order, [title]: { ...order[title], half: half + 1 } } })
         setPriceTotal(price => price += priceMed)
-    }
 }
 
 export const substractHalf = ({ basic, half, setHalf }: propsAddSubstractHalf) => {
