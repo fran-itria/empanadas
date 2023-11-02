9177ef23aaa9eb91e2a89f376cc62767ef732cc4
Franco Itria <francoitria01@gmail.com>
2023-11-02 15:21:53 -0300

    Change type string for number

--- src/services/transformObjectToString.ts
+++ src/services/transformObjectToString.ts
@@ -2,15 +2,11 @@ import type { propsTransformObject } from "./typesServices"
 
 export const transformObjectToString = ({ order, setOrderTextParam, priceTotal }: propsTransformObject) => {
     const keys: string[] = Object.keys(order)
-    const values: { doc: string, half: string }[] = Object.values(order)
+    const values: { doc: number, half: number }[] = Object.values(order)
     values.forEach((value, i) => {
         if (Number(value.doc) > 0 || Number(value.half) > 0) {
             setOrderTextParam(orderTextParam => orderTextParam + `${keys[i]}=${value.doc}doc,${value.half}med-`)
         }
     })
-    // for (const [key, value] of Object.entries(order)) {
-    //     const doc:  doc = value.
-    //     setOrderTextParam(orderTextParam => orderTextParam + `${key}=${textValues.doc}doc${textValues.half}med-`)
-    // }
     setOrderTextParam(orderTextParam => orderTextParam + `price=${priceTotal}`)
 }
